e66b48b1ff38af6577d3e6c5d589d52ffbec4483
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2024-03-21 00:13:21 +0100

    update

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const getCurrentLocation = () => {
 const showLocation = async (position) => {
 
     try {
-        const response = await fetch(`https://api.opencagedata.com/geocode/v1/json?key=98433bc618844a1eb558ba08394dbc51&q=${position.coords.latitude}+${position.coords.longitude}&pretty=1&no_annotations=1`);
+        const response = await fetch(`https://api.opencagedata.com/geocode/v1/json?key=98433bc618844a1eb558ba08394dbc51&q=${position.coords.latitude}+${position.coords.longitude}&pretty=1&no_annotations=4`);
 
         const data = await response.json();
         console.log(data);
