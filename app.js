01ea53540b75d6afb77f71188779da9824bb9146
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2024-03-21 01:09:16 +0100

    updated

--- app.js
+++ app.js
@@ -17,12 +17,15 @@ const getCurrentLocation = () => {
 const showLocation = async (position) => {
 
     try {
-        const response = await fetch(`https://api.opencagedata.com/geocode/v1/json?key=98433bc618844a1eb558ba08394dbc51&q=${position.coords.latitude}+${position.coords.longitude}&pretty=1&roadinfo=1&no_annotations=1&address_only=1`);
+        // const response = await fetch(`https://api.opencagedata.com/geocode/v1/json?key=98433bc618844a1eb558ba08394dbc51&q=${position.coords.latitude}+${position.coords.longitude}&pretty=1&roadinfo=1&no_annotations=1&address_only=1`);
+        const response = await fetch(`http://api.positionstack.com/v1/reverse?access_key=2a27b4745240196a4c034c5d525cfd27&query= ${position.coords.latitude},${position.coords.longitude}&country_module=1`);
 
         const data = await response.json();
         console.log(data);
 
-        locationResult.innerText = `${data.results[0].formatted}`;
+        // locationResult.innerText = `${data.results[0].formatted}`;
+
+        locationResult.innerText = `${data.data[0].label}`;
 
     } catch (error) {
         switch (error.code) {
@@ -42,4 +45,4 @@ const showLocation = async (position) => {
 }
 
 
-currentLocationBtn.addEventListener("click", getCurrentLocation)
+currentLocationBtn.addEventListener("click", getCurrentLocation);
